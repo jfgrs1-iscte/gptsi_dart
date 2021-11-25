a287718350834a4fe81f713f8266198f70166899
jfgrs1-iscte <jfgrs1@iscte-iul.pt>
2021-11-25 18:10:03 +0000

    Login & Registar

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,13 @@ class HomeScreen extends StatelessWidget {
               child: Image.asset('assets/transp.png'),
             ),
             RaisedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => RegisterScreen()),
+                );
+              },
               padding: EdgeInsets.symmetric(horizontal: 100.0),
               child: Text(
                   'FAZER REGISTO',
@@ -209,6 +215,17 @@ class LoginScreen extends StatelessWidget {
             color: Colors.black,
             ),
           ),
+          SizedBox(
+            height: 40.0,
+          ),
+          Center(
+            child: Text('Ao Continuar está a concordar com os Termos e Condições',
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey[600],
+              ),
+            ),
+          ),
           ],
         ),
       ),
@@ -216,3 +233,138 @@ class LoginScreen extends StatelessWidget {
   }
 }
 
+class RegisterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Padding(
+        padding: EdgeInsets.all(3),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child:Container(
+                    child: BackButton(
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 7,
+                  child:Container(
+                    child: SizedBox(
+                      width: 20.0,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child:Container(
+                    padding: EdgeInsets.all(1.0),
+                    child: Image.asset('assets/transp.png'),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+              child: Text('Username:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 0.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(
+                  ),
+                  labelText: 'Introduzir Username',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+              child: Text('E-Mail:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 0.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(
+                  ),
+                  labelText: 'Introduzir E-Mail',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20.0, 40.0, 0.0, 0.0),
+              child: Text('Password:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 0.0),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(
+                  ),
+                  labelText: 'Introduzir Password',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(70.0, 15.0, 0.0, 0.0),
+              child: RaisedButton(
+                onPressed: () {},
+                padding: EdgeInsets.symmetric(horizontal: 100.0),
+                child: Text(
+                  'REGISTAR',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14.0,
+                    letterSpacing: 1.0,
+                  ),
+                ),
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Center(
+            child: Text('Ao Continuar está a concordar com os Termos e Condições',
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey[600],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
